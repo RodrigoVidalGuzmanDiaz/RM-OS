612fdd0b0a3da6e1604de26566cea795f610233d
roddes <rvguzman@scl.edu.gt>
2022-05-24 08:23:36 -0600

    Cambio de posicion en la barra de tareas

--- js/main.js
+++ js/main.js
@@ -1,9 +1,36 @@
-var menu_deplegado = document.getElementById('menu_deplegado');
-var ico_menu = document.getElementById('ico_menu');
-
 //Funcion 
-function clickIco_menu(){
+function desplegarMenu(){
+    /*variables que harán la accion*/
+    var ico_menu = document.getElementById('ico_menu');
+    var ico_menu2  = document.getElementById('ico_menu2');
+    /*variables que reciben la accion*/
+    var menu_opciones_desplegado = document.getElementById('menu_opciones_desplegado');
+    var menu_deplegado = document.getElementById('menu_deplegado');
+    /*clases agregadas a las variables*/
+    menu_opciones_desplegado.classList.add('activarFlex')
     menu_deplegado.classList.add('activar');
+
+    ico_menu.classList.add('desactivar')
+    ico_menu2.classList.add('activarBoton');
 }
-//Evento
-ico_menu.addEventListener('click', clickIco_menu);
+function cerrarMenu(){
+    /*variables que harán la accion*/
+    var ico_menu = document.getElementById('ico_menu');
+    var ico_menu2  = document.getElementById('ico_menu2');
+    /*variables que reciben la accion*/
+    var menu_opciones_desplegado = document.getElementById('menu_opciones_desplegado');
+    var menu_deplegado = document.getElementById('menu_deplegado');
+    /*clases desagregadas a las variables*/
+    menu_opciones_desplegado.classList.remove('activarFlex')
+    menu_deplegado.classList.remove('activar');
+    
+    ico_menu.classList.remove('desactivar')
+    ico_menu2.classList.remove('activarBoton');
+}
+function explorador(){
+
+    var ico_explo = document.getElementById('ico_explo')
+    
+
+}
+
